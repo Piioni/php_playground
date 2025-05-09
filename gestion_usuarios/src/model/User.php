9632007d5ec08f9e2a60ad0f982513fb68c27010
Piioni <rangelc.juane@gmail.com>
2025-05-09 17:50:04 +0200

    Feature: User model for CRUD

---
+++ gestion_usuarios/src/model/User.php
@@ -0,0 +1,45 @@
+<?php
+require_once __DIR__ . '/../config/Database.php';
+class User {
+    private $pdo;
+
+    public function __construct() {
+        $this->pdo = Database::getInstance();
+    }
+
+    public function create($name, $username, $email, $password) {
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+        $stmt = $this->pdo->prepare("INSERT INTO users (name, username, email, password) VALUES ( ? ? ?)");
+        return $stmt->execute([$name, $email, $passwordHash]);
+    }
+
+    public function findAll() {
+        $stmt = $this->pdo->query("SELECT * FROM users");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function findById($id) {
+        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function findByEmail($email) {
+        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE email  =?");
+        $stmt->execute([$email]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function findByUsername($username) {
+        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE username = ?");
+        $stmt->execute([$username]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function findByEmailOrUsername($identifier) {
+        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE email = ? OR username = ?");
+        $stmt->execute([$identifier, $identifier]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+}
