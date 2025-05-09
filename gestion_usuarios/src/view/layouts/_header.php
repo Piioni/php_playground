430ae9c508ec45ee24abecd420ab91fa4b215409
Piioni <rangelc.juane@gmail.com>
2025-05-09 17:49:34 +0200

    Feature: header & footer to include in php files

---
+++ gestion_usuarios/src/view/layouts/_header.php
@@ -0,0 +1,13 @@
+<?php
+$title = "";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo htmlspecialchars($title, ENT_QUOTES) ?></title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+
