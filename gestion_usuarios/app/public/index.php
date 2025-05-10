d9d56c4152d01c9dabbcd2d0f66e869a13cc1298
Piioni <rangelc.juane@gmail.com>
2025-05-10 03:26:02 +0200

    Fix: better index page routing

--- gestion_usuarios/app/public/index.php
+++ gestion_usuarios/app/public/index.php
@@ -1,6 +1,23 @@
 <?php
 include(__DIR__ . '/../config/config.php');
 
+function loadView(string $viewPath): void
+{
+    $fullPath = __DIR__ . '/../src/view/pages/' . $viewPath;
+
+    if (!file_exists($fullPath)) {
+        throw new RuntimeException("View file not found: $viewPath");
+    }
+
+    include $fullPath;
+}
+
+// Obtener la ruta solicitada y eliminar la parte base de la URL
+
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$pathInfo = str_replace(base_url, '', $requestUri);
+$pathInfo = rtrim($pathInfo, '/') ?: '/';  // Normaliza rutas vacías a '/'
+
 
 $urlMap = [
     '/' => 'homepage.php',
@@ -11,14 +28,9 @@ $urlMap = [
 ];
 
 
-// Obtener la ruta solicitada
-$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$pathInfo = str_replace(base_url, '', $requestUri);
-
 if (isset($urlMap[$pathInfo])) {
-    include(__DIR__ . '/../src/view/pages/' . $urlMap[$pathInfo]);
+    loadView($urlMap[$pathInfo]);
 } else {
-    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
-    include(__DIR__ . '/../src/view/pages/404.php');
-    exit;
+    loadView('404.php');
+    http_response_code(404);
 }
