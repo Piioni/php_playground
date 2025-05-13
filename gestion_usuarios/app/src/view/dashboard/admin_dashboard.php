8b78ea079377fb5dcfa12b0574b068d6deddefbd
Piioni <rangelc.juane@gmail.com>
2025-05-13 21:07:54 +0200

    To-Do: Admin dashboard

--- gestion_usuarios/app/src/view/dashboard/admin_dashboard.php
+++ gestion_usuarios/app/src/view/dashboard/admin_dashboard.php
@@ -10,25 +10,6 @@ $title = 'Admin Dashboard';
 include(__DIR__ . '/../layouts/_header.php');
 ?>
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header text-center">
-                        <h4>Panel de Administración</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="d-flex justify-content-center">
-                            <div class="card mb-3" style="width: 18rem;">
-                                <div class="card-body text-center">
-                                    <h5 class="card-title" >Usuarios</h5>
-                                    <p class="card-text">Gestiona los usuarios de la aplicación.</p>
-                                    <a href="<?php echo '/admin/users'; ?>" class="btn btn-primary">Ir a
-                                        Usuarios</a>
-                                </div>
-                            </div>
-
-
 
 
 <?php
